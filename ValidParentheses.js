759b61e782b8a11c249578a235f14593a9fc5d9e
Oger Carrillo <ogercarrillo@gmail.com>
2023-04-10 11:43:27 -0700

    initial commit

---
+++ ValidParentheses.js
@@ -0,0 +1,40 @@
+// "()"
+// "()[]{}"
+// "(]" 
+// "(){}}{" "
+
+let case1 = "()"; 
+let case2 = "()[]{}"; 
+let case3 = "(]"; 
+let case4 = "(){}}{"; 
+
+var isValid = function(s) {
+
+    let check1 = false; 
+    let check2 = false; 
+    let check3 = false; 
+
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] == "(" && s[i+1] == ")"){
+            check1 = true; 
+        }
+        if(s[i] == "[" && s[i+1] == "]"){
+            check2 = true; 
+        }
+        if(s[i] == "{" && s[i+1] == "}"){
+            check3 = true; 
+        }
+    }
+
+    if(check1 == true && check2 == true && check3 == true) {
+        return true; 
+    }
+    else{
+        return false; 
+    }
+}
+
+console.log(isValid(case1)); 
+console.log(isValid(case2));
+console.log(isValid(case3));
+console.log(isValid(case4));
